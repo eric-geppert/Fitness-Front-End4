ca9e766efef8b3ee8efca5b98f2697f5d793675f
Eric <ericgeppert@att.net>
2019-08-02 15:33:30 -0500

    Updates

---
+++ precache-manifest.d5922957af17e0355c3c8c43b826b102.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fd300d6f313b141717b7d64ac26e6c9",
+    "url": "/Fitness-Front-End4/static/media/ConditioningWeightLoss4V1.6fd300d6.pdf"
+  },
+  {
+    "revision": "91608278217016f97cd5",
+    "url": "/Fitness-Front-End4/static/css/main.84802ed1.chunk.css"
+  },
+  {
+    "revision": "283d9a10f713a552c44d",
+    "url": "/Fitness-Front-End4/static/js/runtime~main.4004fa8e.js"
+  },
+  {
+    "revision": "4b5640844d59b8f1049a",
+    "url": "/Fitness-Front-End4/static/js/2.cd3c6024.chunk.js"
+  },
+  {
+    "revision": "b844ecd301f6004b2865f2418f46e2b7",
+    "url": "/Fitness-Front-End4/static/media/blackLogo.b844ecd3.png"
+  },
+  {
+    "revision": "21ee1fc6272a994b1d417e1511d795ab",
+    "url": "/Fitness-Front-End4/static/media/findMyWorkoutPlan.21ee1fc6.PNG"
+  },
+  {
+    "revision": "4143f135ef0d3ab217b1ac45bfada226",
+    "url": "/Fitness-Front-End4/static/media/Aaidan.4143f135.jpeg"
+  },
+  {
+    "revision": "890c8f257042f4fb596427b5b8caa39f",
+    "url": "/Fitness-Front-End4/static/media/Chase.890c8f25.JPG"
+  },
+  {
+    "revision": "44b49cab976486b0c73c4fd6455bfbec",
+    "url": "/Fitness-Front-End4/static/media/Katya.44b49cab.jpg"
+  },
+  {
+    "revision": "271e857a6076629d18b567ed4fbc454c",
+    "url": "/Fitness-Front-End4/static/media/Colton2.271e857a.jpg"
+  },
+  {
+    "revision": "6c36d9f735bc1b347bd1318b717c49b7",
+    "url": "/Fitness-Front-End4/static/media/ConditioningWeightLoss5V1.6c36d9f7.pdf"
+  },
+  {
+    "revision": "91608278217016f97cd5",
+    "url": "/Fitness-Front-End4/static/js/main.94428b5e.chunk.js"
+  },
+  {
+    "revision": "d6ab01d41126b6d4cd744be6fbdd1688",
+    "url": "/Fitness-Front-End4/static/media/ConditioningWeightLoss3V1.d6ab01d4.pdf"
+  },
+  {
+    "revision": "cf166b5d08a32b681947534eba656f7f",
+    "url": "/Fitness-Front-End4/static/media/AtHomeTotalBody5V1.cf166b5d.pdf"
+  },
+  {
+    "revision": "4ccb111add6abf2cad0879f09357132f",
+    "url": "/Fitness-Front-End4/static/media/AtHomeTotalBody4V1.4ccb111a.pdf"
+  },
+  {
+    "revision": "5d89ce4492216418ead5d04a40eccdd7",
+    "url": "/Fitness-Front-End4/static/media/AtHomeTotalBody3V1.5d89ce44.pdf"
+  },
+  {
+    "revision": "b2945e31518df4685d037fa76974dbba",
+    "url": "/Fitness-Front-End4/static/media/FunctionalFitnessHIIT5V1.b2945e31.pdf"
+  },
+  {
+    "revision": "4a92fac94659e0882fc7624859e86409",
+    "url": "/Fitness-Front-End4/static/media/FunctionalFitnessHIIT4V1.4a92fac9.pdf"
+  },
+  {
+    "revision": "cc3c4d2abe8930e8a73a1c3e0203c427",
+    "url": "/Fitness-Front-End4/static/media/FunctionalFitnessHIIT3V1.cc3c4d2a.pdf"
+  },
+  {
+    "revision": "c30e3de0bd04260e891174bbdc72aae6",
+    "url": "/Fitness-Front-End4/static/media/TotalBodyTransformation5V1.c30e3de0.pdf"
+  },
+  {
+    "revision": "b3a1b714ff4c846e540901b46ca7590f",
+    "url": "/Fitness-Front-End4/static/media/TotalBodyTransformation4V1.b3a1b714.pdf"
+  },
+  {
+    "revision": "ca2ab5f05880a6ff85c2dc9fc44f9b9f",
+    "url": "/Fitness-Front-End4/static/media/TotalBodyTransformation3V1.ca2ab5f0.pdf"
+  },
+  {
+    "revision": "ee0055f267ef565f923efe77627dd24b",
+    "url": "/Fitness-Front-End4/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Fitness-Front-End4/precache-manifest.d5922957af17e0355c3c8c43b826b102.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Fitness-Front-End4/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
